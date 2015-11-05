ad4de4f07639b4069093eca0868cf2bd580d8cc8
Lauren Beatty <lauren.m.beatty@gmail.com>
2015-11-05 09:31:10 -0600

    added minified files

---
+++ public/javascripts/client.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Lauren on 11/5/15.
+ */
+$(document).ready(function(){
+    event.preventDefault();
+
+    $('.button-collapse').sideNav({
+            menuWidth: 240, // Default is 240
+            edge: 'left', // Choose the horizontal origin
+            closeOnClick: true // Closes side-nav on <a> clicks, useful for Angular/Meteor
+        }
+    );
+
+});
