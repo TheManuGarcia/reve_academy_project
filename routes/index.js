6db48984e560e375013d4948e28be629966b8258
Lauren Beatty <lauren.m.beatty@gmail.com>
2015-11-05 13:49:14 -0600

    added new routes and views

--- routes/index.js
+++ routes/index.js
@@ -5,6 +5,8 @@ var username = 'Lauren';
 router.get('/', function (req, res, next) {
     res.render('index', {title: 'Rêve Academy Application', username:username});
 });
+
+//SKILLS ROUTES
 router.get('/communication', function (req, res, next) {
     res.render('communication', {title: 'Communication page'});
 
@@ -35,6 +37,47 @@ router.get('/supportive_learning', function (req, res, next) {
 });
 router.get('/responsibility', function (req, res, next) {
     res.render('responsibility', {title: 'responsibility'});
+});
+
+router.get('/observation', function (req, res, next) {
+    res.render('observation', {title: 'Observation'});
+});
+
+router.get('/login', function (req, res, next) {
+    res.render('login', {title: 'Login Page'});
+});
+
+router.get('/register', function (req, res, next) {
+    res.render('register', {title: 'Register'});
+});
+
+
+//ADMIN ROUTES
+
+router.get('/add_intern', function (req, res, next) {
+    res.render('/admin/add_intern', {title: 'Add Intern'});
+});
+
+router.get('/add_teacher', function (req, res, next) {
+    res.render('/admin/add_teacher', {title: 'Add Teacher'});
+});
+
+router.get('/admin_home', function (req, res, next) {
+    res.render('/admin/admin_home', {title: 'Admin Home'});
+});
+
+router.get('/admin_view_data', function (req, res, next) {
+    res.render('/admin/admin_view_data', {title: 'Admin View Data'});
+});
+
+//TEACHER'S ROUTES
+
+router.get('/add_class', function (req, res, next) {
+    res.render('/teacher/add_class', {title: 'Add Class'});
+});
+
+router.get('/teacher_view_data', function (req, res, next) {
+    res.render('/teacher/teacher_view_data', {title: 'Teacher View Data'});
 });
 
 
