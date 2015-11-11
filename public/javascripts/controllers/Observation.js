a68798140759b830c3161226d59df2cc94fc7c81
GridTrekkor <jwo@umn.edu>
2015-11-11 15:43:18 -0600

    Obs updates

--- public/javascripts/controllers/Observation.js
+++ public/javascripts/controllers/Observation.js
@@ -8,12 +8,10 @@ app.controller('ObservationController', function($http) {
     observation.obsSaved = false;
 
     $http.get('/getClasses').then(function (data) {
-        //console.log(data.data);
         observation.Classes = data.data;
     });
 
     observation.selectClass = function (selectedClass) {
-        //console.log(selectedClass.ClassID);
         observation.classClicked = true;
         observation.classSelected = selectedClass.ClassName;
         observation.getStudents(selectedClass.ClassID);
@@ -23,16 +21,14 @@ app.controller('ObservationController', function($http) {
     observation.getStudents = function (classID) {
 
         $http.get('/getStudents/' + classID).then(function (data) {
-            //console.log("students = ", data);
             observation.studentData = data.data;
-            //$('.respo').prop('checked', true);
         });
     };
 
     observation.selectStudent = function (student){
         observation.studentClicked = true;
         observation.studentSelected = student;
-        //console.log(student);
+        resetSliders();
     };
 
     observation.formData = [];
@@ -59,41 +55,29 @@ app.controller('ObservationController', function($http) {
 
     /////////////////////// SLIDER FORM ///////////////////////
 
-    observation.resetSliders = function() {
+    function resetSliders() {
         observation.sliderCommunication = 0;
         observation.sliderEnthusiasm = 0;
         observation.sliderTeamwork = 0;
         observation.sliderProblemSolving = 0;
         observation.sliderProfessionalism = 0;
-    };
+    }
 
-    observation.changeSlider = function(slider, ObsValue) {
-
-        //Obs = {ObsValue : parseInt(ObsValue)};
-
-        //return $http.post('/addObs', Obs);
-
-        console.log("slider " + slider + " = " + parseInt(ObsValue));
-    };
-
-    observation.resetSliders();
     observation.sliderData = {};
 
     observation.saveSliderObs = function() {
-
-        //console.log(observation.studentSelected.StudentID);
-
         observation.sliderData.StudentID = parseInt(observation.studentSelected.StudentID);
         if(parseInt(observation.sliderCommunication) !=0)observation.sliderData.Communication = parseInt(observation.sliderCommunication);
         if(parseInt(observation.sliderEnthusiasm) !=0)observation.sliderData.Enthusiasm = parseInt(observation.sliderEnthusiasm);
         if(parseInt(observation.sliderTeamwork) !=0)observation.sliderData.Teamwork = parseInt(observation.sliderTeamwork);
         if(parseInt(observation.sliderProblemSolving) !=0)observation.sliderData.ProblemSolving = parseInt(observation.sliderProblemSolving);
         if(parseInt(observation.sliderProfessionalism) !=0)observation.sliderData.Professionalism = parseInt(observation.sliderProfessionalism);
-        //console.log(observation.sliderData);
 
+        observation.obsSaved = true;
+        observation.message = "Your observation was saved.";
+
+        $("#observationButton").prop('disabled', true).remove();
         return $http.post('/addObsSlider', observation.sliderData);
-
-
     }
 
 });
